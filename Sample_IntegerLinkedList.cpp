2169acc8d07864885b77d68309ea8e556860c02a
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-08-23 21:45:58 -0700

    SAMPLE PROBLEM 2

---
+++ Sample_IntegerLinkedList.cpp
@@ -0,0 +1,37 @@
+// ADD ANSWER TO THIS FILE
+
+#include "IntegerLinkedList.h"
+
+int IntegerLinkedList::getSum() { // COMPLETE THIS FOR PROBLEM 2
+SNode* ptr = head;
+int sum = 0;
+while (ptr != nullptr) {
+  sum += ptr->elem;
+  ptr = ptr-> next;
+}
+return sum;
+}
+/* another way:
+int sum = 0;
+while(head != nullptr) {
+sum += head->elem;
+head = head->next;
+}
+return sum;
+*/
+
+int IntegerLinkedList::getSumRecurse (SNode *ptr) {
+  return -1; // COMPLETE THIS FOR PROBLEM 3
+}
+
+void IntegerLinkedList::addFront(int x) {
+  SNode *tmp = head;
+	head = new SNode;
+	head->next = tmp;
+	head->elem = x;
+}
+
+// recursion helper function called from main for PROBLEM 3
+int IntegerLinkedList::getSumRecurseHelper () {
+  return getSumRecurse(head);
+}
