b89c6d6dd67f287ec978a90d8028f496d3c5ae30
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-08-29 21:42:52 -0700

    EPP SPRING 2021 PART 2

--- CatsRule.h
+++ CatsRule.h
@@ -1,47 +1,23 @@
 /* Definding classes. checkIn function that will keep track of which pets are waiting and
 seePet function that will return the name of the pet who is next.*/
-#ifndef CatsRule
+#prgma once
 //#define CatsRule
 #include <string>
 #include <iostream>
 #include <stdio.h>
 #include <iomanip>
+#include <unordered_map>
 using namespace std;
 
 class CatsRule
 {
-private:
-  string species_;
-  string name_;
-
 public:
-  CatsRule(string species, string name)
-  {
-    species_ = species;
-    name_ = name;
-  }
+  CatsRule();
+  void checkIn(string name, string species); // will create a list that will add the species and the name
+  bool seePet(string name); // check if the name is in the list
 
-  void checkIn(string species, string name)
-  {
-    species_ = species;
-    name_ = name;
-  }
-
-  string getSpecies()
-  {
-    return species_;
-  }
-
-  string getName()
-  {
-    return name_;
-  }
-
-  string seePet()
-  {
-    return species_;
-    return name_;
-  }
+private:
+  unorderedmap<string, string> pets;
 };
 
 #endif
